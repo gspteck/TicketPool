4f0fc2056fcd95cef989fb0c5f3de04e30195d83
gspteck <66552124+gspteck@users.noreply.github.com>
2020-10-06 09:08:04 +0200

    random edits

--- assets/app.js
+++ assets/app.js
@@ -6,5 +6,10 @@ tl.to('.slider', {y:"-100%", duration: 1.5, delay: 0.5});
 tl.to('.intro', {y:"-100%", duration: 1}, "-=1");
 tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
 tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
+tl.fromTo('.home-button', {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
 
 //
+
+function home() {
+    window.open("./index.html","_self")
+}
