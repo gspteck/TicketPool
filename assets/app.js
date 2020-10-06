19df9d579f7fd2bddb38be5d5e851d3ecc4b94ec
gspteck <66552124+gspteck@users.noreply.github.com>
2020-10-06 14:58:29 +0200

    random edits

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,12 @@
 const tl = gsap.timeline({defaults:{ease: 'power1.out'}});
 
-//main
-tl.to('.text', {y:"0%", duration: 1, stagger: 0.25});
-tl.to('.slider', {y:"-100%", duration: 1.5, delay: 0.5});
-tl.to('.intro', {y:"-100%", duration: 1}, "-=1");
-tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
-tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
-tl.fromTo('.big-text-box', {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
-tl.fromTo('.home-button', {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
+function home_load() {
+    tl.to('.text', {y:"0%", duration: 1, stagger: 0.25});
+    tl.to('.slider', {y:"-100%", duration: 1.5, delay: 0.5});
+    tl.to('.intro', {y:"-100%", duration: 1}, "-=1");
+    tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1});
+    tl.fromTo('.big-text', {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
+    tl.fromTo('.big-text-box', {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
+    tl.fromTo('.home-button', {opacity: 0}, {opacity: 1, duration: 1}, "-=1");
+}
 
-//
